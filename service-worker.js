8e62dfdad72352ef7f6ce5d133646187cd789af3
ctestttest27-cpu <ctestttest27@gmail.com>
2026-02-12 03:16:48 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,30 @@
-const CACHE_NAME = "vida-saudavel-cache-v1";
+const CACHE_NAME = "app-cache-v2";
 
 const urlsToCache = [
-  "/cardapio-app/",
-  "/cardapio-app/index.html",
-  "/cardapio-app/manifest.json",
-  "/cardapio-app/icon-192.png",
-  "/cardapio-app/icon-512.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-// Instalação
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Ativação
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
   );
 });
 
-// Fetch (offline)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
